f34dd8f4b846df05873cddf224647460f8ae0b7d
cherry-002 <alirezaesmaili6040@gmail.com>
2024-11-27 03:19:51 +0330

    config file

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello world";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vgu_buffer = 0xb8000 as *mut u8;
+    
 }
